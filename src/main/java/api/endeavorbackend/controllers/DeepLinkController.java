e783a47a53fcf3b92896dedd5161961dd86b7653
ErickMartins153 <121458723+ErickMartins153@users.noreply.github.com>
2025-06-08 13:20:08 -0300

    feat: adicionar redirecionamento para deep linking

---
+++ src/main/java/api/endeavorbackend/controllers/DeepLinkController.java
@@ -0,0 +1,24 @@
+package api.endeavorbackend.controllers;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+public class DeepLinkController {
+
+    @GetMapping("/api/abrir-grupo/{id}")
+    public ResponseEntity<Void> abrirGrupo(@PathVariable String id) {
+        URI deeplinkUri = URI.create("endeavor://grupos/" + id);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(deeplinkUri);
+
+        return new ResponseEntity<>(headers, HttpStatus.FOUND);
+    }
+}
